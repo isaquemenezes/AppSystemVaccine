8b71c07b600f87d65b79c1c75c8b1f3eef65f36a
Isaque Menezes da Silva <37957188+isaquemenezes@users.noreply.github.com>
2021-08-24 10:44:24 -0300

    update

--- App/routes.py
+++ App/routes.py
@@ -2,7 +2,7 @@ from App import app
 from flask import render_template, redirect, request, url_for
 import pymysql as pys
 
-#DataScience--------
+#-----DataScience--------
 import requests
 import pandas as ps
 
@@ -12,7 +12,9 @@ connection = pys.connect(db='health_card', user='root', password='')
 #Instanciando um cursor
 cursor = connection.cursor()
 
+#--------------------------------------
 #Page INICIAL
+#--------------------------------------
 @app.route('/')
 @app.route('/home')
 def home_page():
@@ -28,6 +30,7 @@ def vaccine_register_page():
     #Select de vacinas disponíveis para cadastrar
     sql = "SELECT * FROM vaccine_card"
     cursor.execute(sql)
+
     query = cursor.fetchall()
     list_vaccine = query
    
@@ -62,15 +65,16 @@ def show_page():
     #Select de dados()
     sql = "SELECT * FROM vaccine_user"
     cursor.execute(sql)
+
     query = cursor.fetchall()
 
     return render_template('vaccine_show.html', query=query) 
 
 #------------------------------------------
-#Page notification
+#Page Show notification
 #-----------------------------------------
 @app.route('/notification')
-def add_notification_page():
+def notification_page():
 
     sql_notif = "SELECT * FROM list_notification"
     cursor.execute(sql_notif)
@@ -94,16 +98,30 @@ def add_notification():
         __date = (email, date, phone)
 
         cursor.execute(__query, __date)
-
         connection.commit()
 
         return redirect(url_for('add_notification_page'))
 
+#-------------------------------------------------------
+#Delete notification
+#-------------------------------------------------------
+@app.route('/delnotfication/<int:id>')
+def __delnotification(id):
+
+    __del_query = "DELETE FROM list_notification WHERE id=%s"
+
+    cursor.execute(__del_query, id)
+    connection.commit()
+
+    return redirect(url_for('notification_page'))
+
+
 #-----------------------------------------------------
-#Edit
+#Edit vaccine user
 #-----------------------------------------------------
 @app.route('/edit/<int:id>')
 def edit_view(id):
+
     query = "SELECT * FROM vaccine_user WHERE id=%s"
 
     cursor.execute(query, id)
@@ -115,30 +133,29 @@ def edit_view(id):
 
 
 #-----------------------------------------------
-#Update
+#Update vaccine user
 #-----------------------------------------------
 @app.route('/update', methods = ['GET', 'POST'])
 def __update():
 
     if request.method == 'POST':
 
-        name = request.form['name']
-        date = request.form['date']
-        batch=request.form['batch']
-        _id  = request.form['id']
+        name = request.form['name']  #name="name"  <- value
+        date = request.form['date']  #name="date"  <- value
+        batch=request.form['batch']  #name="batch" <- value
+        _id  = request.form['id']    #name="id"    <- value
 
         # save edits
         sql_update = "UPDATE vaccine_user SET name=%s, datte=%s, batch=%s WHERE id=%s"
         _data = (name, date, batch, _id) #Dados form name
 
         cursor.execute(sql_update, _data) #execute
-
         connection.commit() #persistencia
 
         return redirect(url_for('show_page'))
 
 #-------------------------------------------
-#delete
+#delete vaccine user
 #-------------------------------------------
 @app.route('/delete/<int:id>')
 def __delete(id):
